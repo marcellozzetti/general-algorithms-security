f8dde6fdf9414a323c1cdd6db489bcbd09fc9a34
marcellozzetti <48164092+marcellozzetti@users.noreply.github.com>
2021-11-29 10:15:57 -0300

    Commit ajustes

---
+++ src/model/InputDTO.java
@@ -0,0 +1,35 @@
+package model;
+
+/**
+ * Created by marcello.ozzetti on 29/11/21.
+ */
+public class InputDTO {
+
+    String operation;
+    int key;
+    int message;
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public void setMessage(int message) {
+        this.message = message;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public int getMessage() {
+        return message;
+    }
+}
